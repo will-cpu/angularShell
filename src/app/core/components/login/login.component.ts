e860c8145dc18626a1438ca3b42e388eab697f30
Will <wllamb1994@gmail.com>
2022-04-23 20:49:45 -0400

    login form finished, ready for service implementation

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  isHidden:boolean = true;
+  isLoading:boolean = false;
 
-  constructor() { }
+  form!: FormGroup;
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
+  onLogin(){
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value)
+  }
 }
