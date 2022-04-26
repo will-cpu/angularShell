177bccc3e98443e0afc7757760954799e8b27ea2
Will <wllamb1994@gmail.com>
2022-04-26 17:26:14 -0400

    imported missing modules

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { CoreComponent } from './core.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,7 +14,7 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent
       },
-      { path: "f1", loadChildren: () => import('../feature/f1/f1.module').then(m => m.F1Module) }
+      { path: "f1", loadChildren: () => import('../feature/f1/f1.module').then(m => m.F1Module), canActivate: [AuthGuard] }
     ]
 
   }
